35597d0d59131608875270defa5fa910218ad20a
shahak morag <shahak.morag@gmail.com>
2022-06-27 14:56:51 +0300

    update shit

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { API } from 'homebridge';
 import { ComputerVolumeAccessory } from './computerVolume';
 
 export = (api: API) => {
+  throw "shit";
   api.registerAccessory('ComputerVolume', ComputerVolumeAccessory);
 };
